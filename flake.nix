a33edfc9e3eee8e07d63fcf02bbac80defaa7959
Alex James <git@alextjam.es>
2024-01-28 16:02:26 -0600

    flake: update nixpkgs & poetry2nix

--- flake.nix
+++ flake.nix
@@ -2,21 +2,16 @@
   description = "A daemon to allocate ProtonVPN port mappings and inform Transmission of said mappings.";
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
   inputs.poetry2nix.url = "github:nix-community/poetry2nix";
   inputs.poetry2nix.inputs.nixpkgs.follows = "nixpkgs";
 
   outputs = { self, nixpkgs, flake-utils, poetry2nix }: {
     overlay = nixpkgs.lib.composeManyExtensions [
-      poetry2nix.overlay
+      poetry2nix.overlays.default
       (final: prev: {
         protonvpn-pmp-transmission = prev.poetry2nix.mkPoetryApplication {
           projectDir = ./.;
-          overrides = prev.poetry2nix.defaultPoetryOverrides.extend (self: super: {
-            py-natpmp = super.py-natpmp.overridePythonAttrs (old: {
-              buildInputs = (old.buildInputs or []) ++ [ super.setuptools ];
-            });
-          });
         };
       })
     ];
